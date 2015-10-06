e2c7a62835c499fd8940f636540bbb3ad8714223
Kelly Becker <kellywerks@gmail.com>
2015-10-06 16:34:36 -0700

    add new list functionality - Rails CRUD Create Lesson

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,23 @@ class ListsController < ApplicationController
     @lists = List.all
     render :index
   end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to  lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name, :description)
+  end
 end
